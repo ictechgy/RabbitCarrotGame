ee1164e5239d1078be90519a745561c499492cc5
ictechgy <ictechgy@gmail.com>
2020-05-16 02:27:10 +0900

    using singleton

--- src/Carrot.java
+++ src/Carrot.java
@@ -2,11 +2,14 @@ import java.awt.*;
 import java.util.Random;
 
 public class Carrot {
-    public static final Rectangle IMG_CARROT = new Rectangle(48, 48);
-    private Image imgCarrot;
-    private int x;
-    private int y;
-    private static Carrot INSTANCE;
+    private static final Rectangle IMG_CARROT = new Rectangle(48, 48);   //당근 이미지 크기
+    private static final int MOV_DIST = 8;    //랜덤으로 이동할 때 이동할 좌표값
+    private Image imgCarrot;    //당근 이미지
+    private int x;  //중심점 x좌표
+    private int y;  //중심점 y좌표
+    private Random random;
+    private int panelWidth;
+    private int panelHeight;
 
     public int getX() {
         return x;
@@ -16,32 +19,43 @@ public class Carrot {
         return y;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    private Carrot(){ } //Singletone access
+
+    private static class LazyCarrotHolder{
+        public static final Carrot INSTANCE = new Carrot();
     }
 
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    private Carrot(){ } //Singletone
-
-    public Carrot getInstance(int panelWidth, int panelHeight){
+    public Carrot getInstance(Toolkit toolkit, int panelWidth, int panelHeight){
         if(INSTANCE == null){
             INSTANCE = new Carrot();
+            INSTANCE.imgCarrot = toolkit.getImage("res/carrot.png");
             INSTANCE.imgCarrot = imgCarrot.getScaledInstance(IMG_CARROT.width, IMG_CARROT.height, Image.SCALE_SMOOTH);
+            random = new Random();
+            this.panelWidth = panelWidth;
+            this.panelHeight = panelHeight;
         }
+        x = random.nextInt(panelWidth-IMG_CARROT.width)+IMG_CARROT.width/2;
+        y = random.nextInt(panelHeight-IMG_CARROT.height)+IMG_CARROT.height/2;      //랜덤위치에 생성
         return INSTANCE;
     }   //carrot은 사용자가 먹었을 시 없어졌다가 새로운 위치에 나타나게 할 건데, 매번 새로운 객체를 만드는 것이 아니라 기존에 생성된 객체를 사용
-    //위치에 대한 정보는 panel의 너비와 높이를 알아야하므로 panel Class에서 설정하도록 한다.
 
     void move(){
-        Random random = new Random();
         int direction = random.nextInt(4);  //0 ~ 3까지의 난수를 생성한다.
         switch (direction){
-            case 0 -> y -= IMG_CARROT.height; break;
-            case 1 -> x += IMG_CARROT.width; break;
-            case 2 ->
+            case 0 -> y -= MOV_DIST;   //북
+            case 1 -> x += MOV_DIST;    //동
+            case 2 -> y += MOV_DIST;   //남
+            case 4 -> x -= MOV_DIST;    //서
+        }
+
+        if(x >= panelWidth+IMG_CARROT.width/2){   //당근 이미지가 오른쪽 화면을 넘어갔다면
+            x = IMG_CARROT.width/2;
+        }else if(x <= -IMG_CARROT.width/2){   //당근 이미지가 왼쪽 화면을 넘어갔다면
+            x = panelWidth - IMG_CARROT.width/2;
+        }else if(y >= panelHeight + IMG_CARROT.height/2) {    //당근 이미지가 아래쪽 화면을 넘어갔다면
+            y = IMG_CARROT.height/2;
+        }else{  //위쪽 화면을 넘어간 경우
+            y = panelHeight-IMG_CARROT.height/2;
         }
     }
 
--- src/Rabbit.java
+++ src/Rabbit.java
@@ -1,16 +1,56 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Rabbit {
-    public static final Rectangle IMG_RABBIT = new Rectangle(48, 48);
+    private static final Rectangle IMG_RABBIT = new Rectangle(48, 48);
+    private static final int MOV_DIST = 8;  //사용자가 이동버튼을 눌렀을 시 한번에 이동할 변위 값
     private Image imgRabbit;
     private int x;
     private int y;
+    private static Rabbit INSTANCE;
+    private int panelWidth;
+    private int panelHeight;
 
     public int getX() {
         return x;
     }
-
     public int getY() {
         return y;
     }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    private Rabbit(){}  //singleton
+
+    public Rabbit getInstance(Toolkit toolkit, int panelWidth, int panelHeight){
+        if(INSTANCE == null){
+            INSTANCE = new Rabbit();
+            INSTANCE.imgRabbit = toolkit.getImage("res/rabbit.png");
+            INSTANCE.imgRabbit = imgRabbit.getScaledInstance(IMG_RABBIT.width, IMG_RABBIT.height, Image.SCALE_SMOOTH);
+            this.panelWidth = panelWidth;
+            this.panelHeight = panelHeight;
+        }
+        return INSTANCE;
+    }
+
+    void move(KeyEvent e){   //사용자가 입력한 키 방향에 따라 객체의 좌표값 변경
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_UP -> y -= MOV_DIST;
+            case KeyEvent.VK_RIGHT -> x += MOV_DIST;
+            case KeyEvent.VK_DOWN -> y += MOV_DIST;
+            case KeyEvent.VK_LEFT -> x -= MOV_DIST;
+        }
+
+
+
+    }
+
+
+
 }
